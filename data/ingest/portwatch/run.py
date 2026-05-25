4031e3640950f548b5f5ab907c10ac33def9739e
Tommy Gozali <Tommy.gozali97@gmail.com>
2026-05-25 06:21:07 +0200

    clean implementation unnest json duckdb

--- data/ingest/portwatch/run.py
+++ data/ingest/portwatch/run.py
@@ -3,15 +3,16 @@
 from duckdb import connect
 from httpx import get
 from pandas import DataFrame, concat, Series
-from json import loads, dumps
+from tempfile import gettempdir
+from os import path
+from json import dumps
 from pathlib import Path
 
 cf = Path(__file__)
 data_path = cf.parent.relative_to(cf.parents[3])  # data/ingest/portwatch
 table_name = cf.parent.name
+url = "https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/PortWatch_ports_database/FeatureServer/0/query?outFields=*&where=1=1&f=geojson"
 
-# con.execute("INSTALL spatial")
-# con.execute("LOAD spatial")
 def connection(data_path = data_path):
     con = connect()
     con.execute("INSTALL ducklake")
@@ -22,19 +23,20 @@ def connection(data_path = data_path):
     con.execute("USE lake")
     return con
 
-def flatten_column(df: DataFrame, col: str) -> DataFrame:
-    return concat([df, df.pop(col).apply(Series)], axis=1)
-
-url = "https://services9.arcgis.com/weJ1QsnbMYJlCHdG/arcgis/rest/services/PortWatch_ports_database/FeatureServer/0/query?outFields=*&where=1=1&f=geojson"
-
-def get_data(url: str) -> DataFrame:
-    r = get(url)
-    geojson = loads(dumps(r.json()["features"]))
-    df = DataFrame(geojson)
-    df_clean = df.pipe(flatten_column, "geometry").pipe(flatten_column, "properties")
-    return df_clean
-
 if __name__ == "__main__":
-    df_clean = get_data(url)
-    connection.register("df_clean", df_clean)
-    connection.execute(f"CREATE OR REPLACE TABLE {table_name} AS SELECT * FROM df_clean")
+    r = get(url)
+    tmp = path.join(gettempdir(), "f{table_name}.json").replace("\\", "/")
+    with open(tmp, "w") as f:
+        f.write(dumps(r.json()["features"]))
+
+    conn = connection()
+    data = conn.sql(
+        f"""
+        SELECT unnest(properties) 
+            FROM (
+                    SELECT * 
+                    FROM read_json_auto('{tmp}'))
+                    
+        """)
+    conn.register("data", data)
+    conn.execute(f"CREATE OR REPLACE TABLE {table_name} AS SELECT * FROM data")
